64239828b1bc9ec658398cfc888127c1791f4390
muhammad-abdulloh <clevercoderr@gmail.com>
2023-10-21 22:02:47 +0500

    Add project files.

---
+++ LINQ/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace LINQ
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public float Salary { get; set; }
+        public string? Position { get; set; }
+        public List<string> ProgrammingLanguages { get; set; }
+
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>() 
+            {
+                new Employee { Id = 1001, FirstName = "Rustambek", LastName = "Jurayev", Age = 15, Salary = 200, 
+                        Position = "Junior", ProgrammingLanguages = new List<string> { "C", "Python", "C#", "JavaScrpt" } },
+                new Employee { Id = 1002, FirstName = "Nurmuhammad", LastName = "Sharobiddinov", Age = 20,  Salary = 1500, 
+                        Position = "Middle", ProgrammingLanguages = new List<string> { "C", "Python", "C#", "C++" } },
+                new Employee { Id = 1003, FirstName = "Sevinch", LastName = "Xayriddinova", Age = 19,  Salary = 1200, 
+                        Position = "Strong Junior", ProgrammingLanguages = new List<string> { "C", "Python", "C#", "Pascal", "C++" } },
+                new Employee { Id = 1004, FirstName = "Bahriddin", LastName = "Abdusalomov", Age = 24,  Salary = 1600 , 
+                        Position = "Middle", ProgrammingLanguages = new List<string> { "C", "Python", "C#", "C++" }},
+                new Employee { Id = 1005, FirstName = "Sarvarjon", LastName = "G'ulomjonov", Age = 25,  Salary = 2300, 
+                        Position = "Strong Middle", ProgrammingLanguages = new List<string> { "C", "Python", "C#", "GO" } },
+                new Employee { Id = 1006, FirstName = "Azizbek", LastName = "Oxunov", Age = 17,  Salary = 1600, 
+                        Position = "Middle", ProgrammingLanguages = new List<string> { "C", "Python", "C#", "PHP" } },
+                new Employee { Id = 1007, FirstName = "Muhammad Abdulloh", LastName = "Komilov", Age = 23,  Salary = 1600, 
+                        Position = "Middle", ProgrammingLanguages = new List<string> { "C", "Python", "C#", "JavaScrpt", "TypeScript" } },
+            };
+
+            return employees;
+        }
+    }
+}
---
+++ LINQ/Queries.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Queries
+    {
+        public static void Run()
+        {
+            /* simple query select
+            List<int> numbers = new List<int>() { 2, 3, 4,3,4, 4,3,4,34,1,4,1324,1,34,2,432,4,324,32,4,324};
+
+            //Method Syntax
+
+            IEnumerable<int> query = numbers.Where(x => x % 2 == 0).ToList();
+
+
+            //Mixed Syntax
+            List<int> query = (from x in numbers
+                               select x).Where(x => x % 2 == 1).ToList();
+
+            foreach (var number in query)
+            {
+                Console.WriteLine(number);
+            }
+
+
+            // Query Syntax
+
+            var QS = from number in numbers
+                     where number % 2 == 1
+                     select number;
+
+            // query syntax 
+            List<int> QStoList = (from number in numbers
+                                 where number % 2 == 1
+                                 select number).ToList();
+
+            //select* from numbers
+            foreach (var number in query)
+            {
+                Console.WriteLine(number);
+            }
+            */
+        
+            // simple query
+            /**
+            // 1 - ish
+            var employeeSalary1 = from salary in Employee.GetEmployees() 
+                                 select (salary.FirstName, Salary: salary.Salary * 12, salary.Position);
+
+            // 2 - ish
+            var employeeSalary = (from salary in Employee.GetEmployees()
+                                 select new 
+                                 { 
+                                     FirstName = salary.FirstName,
+                                     Salary = salary.Salary * 12,
+                                     Position = salary.Position
+                                 });
+
+            // method syntax
+            var MS = Employee.GetEmployees().Select(x => new
+            {
+                Name = x.FirstName, 
+                Salary = x.Salary * 12,
+                Position = x.Position
+            });
+
+
+            foreach (var employee in employeeSalary1)
+                Console.WriteLine(employee.FirstName +  " " + employee.Position + " " + employee.Salary);
+            */
+
+            List<string> list = new List<string>() { "Muhammad Abdulloh", "Komilov" };
+
+            // method syntax
+            
+            var items = Employee.GetEmployees().SelectMany(x => x.ProgrammingLanguages, 
+                    (parent, child) => new
+                    {
+                        Name = parent.FirstName,
+                        Language = child
+                    }
+           );
+
+
+
+
+            foreach ( var item in items )
+            {
+                Console.WriteLine($"Name: {item.Name}, Language: {item.Language}");
+            }
+            
+
+            // query syntax select many
+
+            //var QS = from employe in list
+            //         from metaItem in employe
+            //         select metaItem;
+
+            //foreach (var item in QS )
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
